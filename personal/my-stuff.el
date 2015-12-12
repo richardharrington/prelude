45731c5707a34c06f95312e00c7d42ff55369e3e
Richard Harrington <harrington.richard@gmail.com>
2015-12-12 11:01:45 -0500

    Create my-stuff.el

---
+++ personal/my-stuff.el
@@ -0,0 +1,41 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+
+;;; Code:
+
+(prelude-require-packages '(ido-vertical-mode))
+
+(define-globalized-minor-mode my-global-ido-vertical-mode ido-vertical-mode
+  (lambda () (ido-vertical-mode 1)))
+
+(my-global-ido-vertical-mode 1)
+
+(define-globalized-minor-mode my-global-linum-mode linum-mode
+  (lambda () (linum-mode t)))
+
+(my-global-linum-mode t)
+
+(defun my-cider-send-last-sexp-to-repl-and-eval ()
+  "inserts into repl, evals there, then returns to original window"
+  (interactive)
+  (save-selected-window
+    (setq current-prefix-arg '(4)) ; C-u
+    (call-interactively 'cider-insert-last-sexp-in-repl)))
+
+(global-set-key (kbd "C-x C-.") 'my-cider-send-last-sexp-to-repl-and-eval)
+
+(defun cider-figwheel-repl ()
+  (interactive)
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/cljs-repl)")
+    (cider-repl-return)))
+
+
+(provide 'my-stuff)
+
+;;; my-stuff.el ends here
